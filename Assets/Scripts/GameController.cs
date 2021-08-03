c1293fbe0712fb42cf3bde98a8ae5d238de212cf
Abed1995 <56524721+Abed1995@users.noreply.github.com>
2021-08-03 22:52:50 +0200

    full project for xo game
    first project i had made in my life and i am very proud of it

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+    private string playerSide;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+    public int moveCount;
+    public GameObject playerXactive;
+    public GameObject playerOactive;
+    public GameObject chooseSidePanel;
+    
+    public GameObject restartButton;
+
+     void Awake()
+    {
+        chooseSidePanel.SetActive(false);
+        restartButton.SetActive(false);
+        gameOverPanel.SetActive(false);
+        SetGameControllerReferenceOnButtons();
+         playerSide = "X";
+        moveCount = 0;
+        changePanels();
+    }
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().setGameControllerReference(this);
+        }
+    }
+   
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        
+        moveCount++;
+          if (moveCount >= 9)
+            setGameOverText("It's Draw");
+        //Raws
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+         GameOver();
+        }
+           
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        //columns
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        //Diagonals
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+       
+
+        changeSide();
+        changePanels();
+    }
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+       
+        setGameOverText(playerSide + "  Wins!");
+  
+
+    }
+
+    void changeSide()
+    {
+        if (playerSide == "X")
+        {
+            playerXactive.SetActive(true);
+            playerOactive.SetActive(false);
+            playerSide = "O";
+        }
+        
+        else if (playerSide == "O")
+        {
+        playerXactive.SetActive(false);
+        playerOactive.SetActive(true);
+            playerSide = "X";
+        }
+            
+        
+    }
+    void setGameOverText(string value)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = value;
+        restartButton.SetActive(true);
+    }
+    public void restartGame()
+    {
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+        chooseSidePanel.SetActive(true);
+       
+        changePanels();
+    }
+    public void changePanels()
+    {
+        if (playerSide == "X")
+        {
+            playerXactive.SetActive(true);
+            playerOactive.SetActive(false);
+        }
+        if (playerSide == "O")
+        {
+            playerXactive.SetActive(false);
+            playerOactive.SetActive(true);
+        }
+    }
+   public void chooseSideX()
+    {
+        playerSide = "X";
+        chooseSidePanel.SetActive(false);
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = true;
+            buttonList[i].text = "";
+        }
+    }
+    public void chooseSideO()
+    {
+        playerSide = "O";
+        chooseSidePanel.SetActive(false);
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = true;
+            buttonList[i].text = "";
+        }
+    }
+}
---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button Button;
+    public Text buttontext;
+    private GameController gameController;
+
+    public void setGameControllerReference(GameController Controller)
+    {
+        gameController = Controller;
+    }
+
+    public void setspace()
+    {
+        buttontext.text = gameController.GetPlayerSide();
+        Button.interactable = false;
+        gameController.EndTurn();
+    }
+}
